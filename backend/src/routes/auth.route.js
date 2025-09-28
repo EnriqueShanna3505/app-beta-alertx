2a488ac12fa7dd34cb49ccd05371e3fd4d15531a
EnriqueShanna3505 <enriqueshanna00@gmail.com>
2025-09-28 17:29:48 +0800

    signup controller added

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,18 +1,16 @@
 //CLEANUP codebase/ Code optimization for routes or endpoints
 
 import express from 'express';
-//import { signup } from '../controllers/auth.controller.js';
+import { signup } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
 //router.post('/signup', signup); //post request to /signup endpoint will be handled by signup function in auth.controller.js, same goes for login and logout
 
+router.post('/signup', signup);
+
 router.get('/login', (req, res) => {
   res.send('This is login endpoint');
-});
-
-router.get('/signup', (req, res) => {
-  res.send('This is signup endpoint');
 });
 
 router.get('/logout', (req, res) => {
