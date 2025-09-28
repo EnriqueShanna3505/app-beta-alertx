ca5df43084a26616b2f021e4eca1eeaf04398d94
EnriqueShanna3505 <enriqueshanna00@gmail.com>
2025-09-28 14:44:43 +0800

    test for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,17 +2,26 @@ import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route.js';
 import messagesRoutes from './routes/message.route.js';
+import path from 'path';
 
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json()); // Middleware to parse JSON request bodies. To get the fields from user send. lets say login, we need username, password, email basically req.body
 
 app.use('/api/auth', authRoutes);
-
 app.use('/api/messages', messagesRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
 
 app.listen(PORT, () => console.log(`Server is running on port:` + PORT));
